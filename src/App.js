fe3190aa4a89d9b83f026fe38ceef93de00f89af
mdemon420 <mdeamon4@gmail.com>
2023-01-31 11:46:17 +0600

    create nav

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import Comments from "./components/comments/Comments";
+import Layout from "./components/layout/Layout";
 import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" exact element={<AllQuotes />} />
-      <Route path='/quotes' element={<AllQuotes />} />
-      <Route path='/quote/:quoteId/*' element={<QuoteDetail />} />
-      <Route path='/new-quote' element={<NewQuote />} />
-      <Route path='/comments' element={<Comments />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" exact element={<AllQuotes />} />
+        <Route path='/quotes' element={<AllQuotes />} />
+        <Route path='/quote/:quoteId/*' element={<QuoteDetail />} />
+        <Route path='/new-quote' element={<NewQuote />} />
+        <Route path='/comments' element={<Comments />} />
+      </Routes>
+    </Layout>
+
   );
 }
 
--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+const DUMMY_QUOTES = [
+
+];
 
 const AllQuotes = () => {
     return (
         <div>
             <h1>All Quotes</h1>
-
         </div>
     );
 };
