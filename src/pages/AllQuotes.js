8097dc403a44e8779d261565c10373de891a6dda
mdemon420 <mdeamon4@gmail.com>
2023-01-31 12:06:07 +0600

    form add

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import QuoteList from '../components/quotes/QuoteList';
 
 const DUMMY_QUOTES = [
-
+    { id: 'p1', author: 'Max', Text: 'Learn React is fun' },
+    { id: 'p2', author: 'Maximilian', Text: 'Learn React is great' },
 ];
 
 const AllQuotes = () => {
     return (
         <div>
             <h1>All Quotes</h1>
+            <QuoteList quotes={DUMMY_QUOTES} />
         </div>
     );
 };
--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import QuoteForm from '../components/quotes/QuoteForm'
 const NewQuote = () => {
+    const addQuoteHandler = (quoteData) => {
+        console.log(quoteData)
+    }
     return (
         <div>
-            <h1>New Quote</h1>
+            <QuoteForm onAddQuote={addQuoteHandler} />
         </div>
     );
 };
