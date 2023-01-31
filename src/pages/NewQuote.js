8a1f155aff627da3cf851faf2b62d18c7d1bc18b
mdemon420 <mdeamon4@gmail.com>
2023-01-31 14:11:33 +0600

    useNavigate use

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import QuoteForm from '../components/quotes/QuoteForm'
 const NewQuote = () => {
+    const navigate = useNavigate();
     const addQuoteHandler = (quoteData) => {
         console.log(quoteData)
+        navigate('/quotes')
     }
     return (
         <div>
