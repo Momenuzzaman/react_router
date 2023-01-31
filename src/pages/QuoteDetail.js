13da828b502cf3d422fe992d3b501d53e1b5f92f
mdemon420 <mdeamon4@gmail.com>
2023-01-31 13:39:09 +0600

    quote detail

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
 import { useParams } from 'react-router-dom';
+import HighLightedQuote from '../components/quotes/HighlightedQuote';
+
+const DUMMY_QUOTES = [
+    { id: 'p1', author: 'Max', text: 'Learn React is fun' },
+    { id: 'p2', author: 'Maximilian', text: 'Learn React is great' },
+];
 const QuoteDetail = () => {
     const params = useParams();
+    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+    if (!quote) {
+        return <p>No quote found</p>;
+    }
     return (
         <div>
-            <h1>Quote Details</h1>
-            {params.quoteId}
+            <HighLightedQuote text={quote.text} author={quote.author} />
             <Routes>
                 <Route path='comment' element={<Comments />} />
             </Routes>
