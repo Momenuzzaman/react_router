81eb6db53a2bd01db12be1e85ec1c8b7f3d4dc0b
mdemon420 <mdeamon4@gmail.com>
2023-02-06 23:59:13 +0600

    query

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,18 +1,24 @@
 import { Fragment } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
 const QuoteList = (props) => {
   const navigate = useNavigate();
+  const location = useLocation()
+
+  const queryParams = new URLSearchParams(location.search);
+  const isSortingAsc = queryParams.get('sort') === 'asc';
+
   const changedSortingHandler = () => {
-    navigate('/quotes?sort=asc');
+    navigate('/quotes?sort=' + (isSortingAsc ? 'desc' : 'asc'));
+    console.log(location);
   };
   return (
     <Fragment>
       <div className={classes.sorting}>
-        <button onClick={changedSortingHandler}>Sort Ascending</button>
+        <button onClick={changedSortingHandler}>Sort {isSortingAsc ? 'Descending' : 'Ascending'}</button>
       </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
