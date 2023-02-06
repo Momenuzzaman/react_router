0b3b7a4b39a85480bca1a3241ceb2d1d8e6ce935
mdemon420 <mdeamon4@gmail.com>
2023-02-06 22:29:43 +0600

    QuoteList add button

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,11 +1,17 @@
 import { Fragment } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
 const QuoteList = (props) => {
+  const navigate = useNavigate();
+  const changedSortingHandler = () => { };
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changedSortingHandler}>Sort Ascending</button>
+      </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
           <QuoteItem
